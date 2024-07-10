682fb5796da8d957660df63c9d33a2709717b4b8
Nikita N <niknechaev345@gmail.com>
2024-07-10 21:20:27 +0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram';
-import Header from './components/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import ProductList from './components/ProductList/ProductList';
-import Form from './components/Form/Form';
+// import Header from './components/Header/Header';
+// import { Route, Routes } from 'react-router-dom';
+// import ProductList from './components/ProductList/ProductList';
+// import Form from './components/Form/Form';
 
 
 function App() {
